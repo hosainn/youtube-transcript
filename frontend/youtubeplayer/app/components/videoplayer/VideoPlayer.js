51293e0638394d91879c334773c7e23356b25f31
Saddam Hosain <hosains738@gmail.com>
2024-12-11 20:12:51 +0600

    frontend: Initial UI for VideoPlayer Component

--- frontend/youtubeplayer/app/components/videoplayer/VideoPlayer.js
+++ frontend/youtubeplayer/app/components/videoplayer/VideoPlayer.js
@@ -1,8 +1,51 @@
-import React from 'react'
+"use client"
+
+import React from 'react';
+import { useState } from 'react';
+import Image from 'next/image'
+import YouTube from "react-youtube";
+
+import "./videoplayer.css";
+
+const getGeneralView = () => {
+    return (
+        <div id="generalViewContainer">
+            <Image
+                src="/youtube.png"
+                width={30}
+                height={30}
+                alt="Picture of the author"
+            />
+            <span id="playerTextContainer">Youtube video player</span>
+        </div>
+
+    )
+}
+
+const getVideoPlayer = (videoId) => {
+    return (
+        <YouTube
+            // onReady={onReady}
+            videoId="UeYmRKWhwFI"
+            opts={{
+                height: "280",   // Set the height
+                width: "600",    // Set the width
+                playerVars: {
+                    autoplay: 1,
+                    mute: 1, //Chrome is blocking autoplay https://developer.chrome.com/blog/autoplay
+                },
+            }}
+        />
+    );
+}
 
 const VideoPlayer = () => {
+    const [videoId, setVideoId] = useState(null)
+
     return (
-        <div>VideoPlayer</div>
+        <div id="videoPlayerContainer">
+            {videoId ? getVideoPlayer(videoId) : getGeneralView()}
+        </div>
     )
 }
 
