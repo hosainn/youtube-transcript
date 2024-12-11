532f83d87053858a9528d77102a979baa638ee2c
Saddam Hosain <hosains738@gmail.com>
2024-12-11 19:25:09 +0600

    frontend: initial VideoInputForm componets UI

--- frontend/youtubeplayer/app/components/videoinputform/VideoInputForm.js
+++ frontend/youtubeplayer/app/components/videoinputform/VideoInputForm.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import "./videoinput.css"
 
 const VideoInputForm = () => {
-  return (
-    <div>VideoInputForm</div>
-  )
+    return (
+        <div id="inputFormContaier">
+            <div id="urlTextContiner">
+                <div><span>Url:</span></div>
+            </div>
+            <div id="inputFieldContainer">
+                <input
+                    id="inputField"
+                    name="myInput"
+                />
+            </div>
+            <div id="submitterTextContainer">
+                <div>
+                    <span className="submitter">
+                        Submit
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default VideoInputForm
--- frontend/youtubeplayer/app/page.js
+++ frontend/youtubeplayer/app/page.js
@@ -1,10 +1,11 @@
 import VideoInputForm from "./components/videoinputform/VideoInputForm";
 import VideoPlayer from "./components/videoplayer/VideoPlayer";
 import TranscriptViewer from "./components/transcriptviewer/TranscriptViewer";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div id={styles.appContainer}>
       <VideoInputForm />
       <VideoPlayer />
       <TranscriptViewer />
