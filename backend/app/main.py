d5a2be942b1100d86917ba60308cc3c7e7b456a7
Saddam Hosain <hosains738@gmail.com>
2024-12-13 12:55:25 +0600

    Initialize transcript api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from youtube_transcript_api import YouTubeTranscriptApi
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
-@app.get("/")
-def index():
-    return {"Hello": "World"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/v1/")
+def index(videoId):
+    res = YouTubeTranscriptApi.get_transcript(videoId)
+    return {"videoId": videoId, "transcript": res}
